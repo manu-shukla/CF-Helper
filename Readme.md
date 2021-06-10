5d8da0d11b8b507a02dec7c55970049e868134d1
Manu Shukla <shukla.manu09@gmail.com>
2021-06-11 01:59:41 +0530

    initial release

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Codeforces Helper
+
+This Website helps you to find the problem with the help of its name. It displays all the problems similar to the user input search and also provides the links to the respective problems.
---
+++ app.js
@@ -0,0 +1,46 @@
+const url = {
+  problems: "https://codeforces.com/api/problemset.problems",
+};
+let probs,
+  check = false;
+function getProblem() {
+  fetch(`${url.problems}`)
+    .then((problems) => problems.json())
+    .then(displayProblems);
+}
+function displayProblems(problems) {
+  console.log(problems);
+  probs = problems;
+  check = true;
+}
+
+getProblem();
+function searchProblem() {
+  if (check == true) {
+    let toSearch = document.getElementById("userinput").value;
+    console.log(toSearch);
+    if (toSearch == "") {
+      alert("Enter a Problem");
+      return;
+    }
+    let arr = probs.result.problems.filter((item) =>
+      item.name.toUpperCase().includes(`${toSearch.toUpperCase()}`)
+    );
+    console.log(arr);
+    let links = arr.map((x) => ({
+      name: x.name,
+      link: `https://codeforces.com/contest/${x.contestId}/problem/${x.index}`,
+    }));
+    let container = document.getElementById("problems");
+    container.innerHTML = "";
+    for (let i of links) {
+      let anchors = document.createElement("a");
+      anchors.href = i.link;
+      anchors.innerText = `${i.name}  `;
+      anchors.style.margin = "10px 8px";
+      container.appendChild(anchors);
+    }
+  } else {
+    alert("Please Wait! Problems Set Not Loaded");
+  }
+}
