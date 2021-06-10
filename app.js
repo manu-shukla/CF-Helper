59d95db09cfa1dff6617b6bdce234a6d89b8bd6b
Manu Shukla <shukla.manu09@gmail.com>
2021-06-11 02:08:32 +0530

    added problemset loading status

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ function getProblem() {
 function displayProblems(problems) {
   console.log(problems);
   probs = problems;
+  document.getElementById("status").innerHTML =
+    "Problem Set Loaded. Now You Can Search";
   check = true;
 }
 
@@ -21,24 +23,24 @@ function searchProblem() {
     console.log(toSearch);
     if (toSearch == "") {
       alert("Enter a Problem");
-      return;
-    }
-    let arr = probs.result.problems.filter((item) =>
-      item.name.toUpperCase().includes(`${toSearch.toUpperCase()}`)
-    );
-    console.log(arr);
-    let links = arr.map((x) => ({
-      name: x.name,
-      link: `https://codeforces.com/contest/${x.contestId}/problem/${x.index}`,
-    }));
-    let container = document.getElementById("problems");
-    container.innerHTML = "";
-    for (let i of links) {
-      let anchors = document.createElement("a");
-      anchors.href = i.link;
-      anchors.innerText = `${i.name}  `;
-      anchors.style.margin = "10px 8px";
-      container.appendChild(anchors);
+    } else {
+      let arr = probs.result.problems.filter((item) =>
+        item.name.toUpperCase().includes(`${toSearch.toUpperCase()}`)
+      );
+      console.log(arr);
+      let links = arr.map((x) => ({
+        name: x.name,
+        link: `https://codeforces.com/contest/${x.contestId}/problem/${x.index}`,
+      }));
+      let container = document.getElementById("problems");
+      container.innerHTML = "";
+      for (let i of links) {
+        let anchors = document.createElement("a");
+        anchors.href = i.link;
+        anchors.innerText = `${i.name}  `;
+        anchors.style.margin = "10px 8px";
+        container.appendChild(anchors);
+      }
     }
   } else {
     alert("Please Wait! Problems Set Not Loaded");
