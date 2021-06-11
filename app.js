15b4b8d24500cf2c07e3ffc66b5387ab5aed5b8c
Manu Shukla <shukla.manu09@gmail.com>
2021-06-11 11:01:33 +0530

    Added realtime search

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const url = {
 };
 let probs,
   check = false;
+  let container = document.getElementById("problems");
 function getProblem() {
   fetch(`${url.problems}`)
     .then((problems) => problems.json())
     .then(displayProblems);
 }
 function displayProblems(problems) {
-  console.log(problems);
   probs = problems;
-  document.getElementById("status").innerText =
-    "Problem Set Loaded. Now You Can Search";
+  document.getElementById("status").innerHTML =
+    "<span>Status: </span>Problem Set Loaded. Now You Can Search";
   check = true;
 }
 
@@ -20,19 +20,17 @@ getProblem();
 function searchProblem() {
   if (check == true) {
     let toSearch = document.getElementById("userinput").value;
-    console.log(toSearch);
-    if (toSearch == "") {
-      alert("Enter a Problem");
-    } else {
-      let arr = probs.result.problems.filter((item) =>
-        item.name.toUpperCase().includes(`${toSearch.toUpperCase()}`)
-      );
-      console.log(arr);
-      let links = arr.map((x) => ({
-        name: x.name,
-        link: `https://codeforces.com/contest/${x.contestId}/problem/${x.index}`,
-      }));
-      let container = document.getElementById("problems");
+
+    let arr = probs.result.problems.filter((item) =>
+      item.name.toUpperCase().includes(`${toSearch.toUpperCase()}`)
+    );
+    console.log(arr);
+    let links = arr.map((x) => ({
+      name: x.name,
+      link: `https://codeforces.com/contest/${x.contestId}/problem/${x.index}`,
+    }));
+    if (toSearch != "") {
+    
       container.innerHTML = "";
       for (let i of links) {
         let anchors = document.createElement("a");
@@ -42,7 +40,15 @@ function searchProblem() {
         container.appendChild(anchors);
       }
     }
+    else{
+      container.innerHTML = "";
+    }
   } else {
     alert("Please Wait! Problems Set Not Loaded");
   }
 }
+
+let input = document.getElementById("userinput");
+input.addEventListener("keyup", function () {
+  searchProblem();
+});
